361299ebe7f5f02c9092229c95c3436eb9e6e626
alfredo_pio <alfredopascualluque7@gmail.com>
2025-06-27 09:39:51 +0200

    Migrations

---
+++ migrations/Version20250625074445.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250625074445 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE email_logs (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, recipient VARCHAR(255) NOT NULL, subject VARCHAR(255) NOT NULL, body CLOB NOT NULL, sent_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+            )
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            DROP TABLE email_logs
+        SQL);
+    }
+}
